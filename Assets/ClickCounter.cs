22868acc357290b5e5410188d77dd21baf2bc220
Ed Paradis <legomaniac@gmail.com>
2015-08-20 22:40:46 -0700

    trivial unity project: a button increments a number

---
+++ Assets/ClickCounter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ClickCounter : MonoBehaviour {
+
+	private Text _label;
+	private int _count;
+
+	// Use this for initialization
+	void Start () {
+		_count = 0;
+		_label = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void IncrementCount()
+	{
+		_count += 1;
+		_label.text = _count.ToString();
+	}
+}
